098cb39ec4384ffe9ddd98fef1aff7562b0f5ffa
Azkal182 <mohazkalarif@gmail.com>
2024-09-30 05:24:15 +0700

    add label and type regencies

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,66 +5,112 @@ import Districts from "./json/kecamatan.json";
 import Villages from "./json/kelurahan.json";
 const prisma = new PrismaClient()
 
+type village = {
+    id: number,
+    name: string,
+    code: string,
+    full_code: string,
+    pos_code: string,
+    kecamatan_id: number
+}
 
 async function main() {
-    console.log("insert provice ...");
-    for (const item of Provinces) {
-        await prisma.province.create({
-            data: {
-                id: item.id,
-                name: item.name,
-                code: item.code
-            }
-        })
-    }
-    console.log('province done');
+    // console.log("insert provice ...");
+    // for (const item of Provinces) {
+    //     await prisma.province.create({
+    //         data: {
+    //             id: item.id,
+    //             name: item.name,
+    //             code: item.code
+    //         }
+    //     })
+    // }
+    // console.log('province done');
 
-    console.log("insert regencies ...");
+    // console.log("insert regencies ...");
 
     for (const item of Regencies) {
-        await prisma.regency.create({
+        const result = await prisma.regency.update({
+            where: { id: item.id },
             data: {
                 id: item.id,
                 name: item.name,
                 code: item.code,
+                label: `${item.type === "Kota" ? "Kota." : "Kab."} ${item.name}`,
+                type: item.type,
                 fullCode: item.full_code,
                 provinceId: item.provinsi_id
             }
         })
+        console.log('berhasil update ', result.label);
+
     }
+    // const formattedRegencies = (Regencies).filter((item: any) => item.id >= 49926).map((item, index) => ({
+    //     id: item.id, // Mengatur ID dimulai dari 49925
+    //     name: item.name,
+    //     label: `${item.type === "kota" ? "Kota. " : "Kab. "} ${item.name}`,
+    //     code: item.code,
+    //     fullCode: item.full_code,
+    //     provinceId: item.provinsi_id
+    // }));
 
-    console.log('regencies done');
 
-    console.log("insert districts ...");
 
-    for (const item of Districts) {
-        await prisma.district.create({
-            data: {
-                id: item.id,
-                name: item.name,
-                code: item.code,
-                fullCode: item.full_code,
-                regencyId: item.kabupaten_id
-            }
-        })
-    }
-    console.log('districts done');
+    // console.log('regencies done');
 
-    console.log("insert village ...");
+    // console.log("insert districts ...");
 
-    for (const item of Villages as any) {
-        await prisma.village.create({
-            data: {
-                id: item.id,
-                name: item.name,
-                code: item.code,
-                fullCode: item.full_code,
-                postalCode: item.pos_code,
-                districtId: item.kecamatan_id
-            }
-        })
-    }
-    console.log('village done');
+    // for (const item of Districts) {
+    //     await prisma.district.create({
+    //         data: {
+    //             id: item.id,
+    //             name: item.name,
+    //             code: item.code,
+    //             fullCode: item.full_code,
+    //             regencyId: item.kabupaten_id
+    //         }
+    //     })
+    // }
+    // console.log('districts done');
+
+    // console.log("insert village ...");
+
+    // for (const item of Villages as any) {
+    //     await prisma.village.create({
+    //         data: {
+    //             id: item.id,
+    //             name: item.name,
+    //             code: item.code,
+    //             fullCode: item.full_code,
+    //             postalCode: item.pos_code,
+    //             districtId: item.kecamatan_id
+    //         }
+    //     })
+    // }
+    // console.log('village done');
+
+    // for (const item of Villages as any) {
+    //     if (item.id >= 49925) {
+    //         console.log(item.id);
+
+    //     }
+    // }
+
+    // const formattedVillages = (Villages as village[]).filter((item: any) => item.id >= 49926).map((item, index) => ({
+    //     id: item.id, // Mengatur ID dimulai dari 49925
+    //     name: item.name,
+    //     code: item.code,
+    //     fullCode: item.full_code,
+    //     postalCode: item.pos_code,
+    //     districtId: item.kecamatan_id,
+    // }));
+
+    // await prisma.village.createMany({
+    //     data: formattedVillages,
+    //     skipDuplicates: true
+    // })
+
+
 
 }
 main()
