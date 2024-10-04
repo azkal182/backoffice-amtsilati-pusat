978e2e4bbead86bfed3a3b9d7d5425ce44a4c2a3
Azkal182 <mohazkalarif@gmail.com>
2024-10-04 22:41:18 +0700

    change trusthost:https://amtsilati.online

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config) => {
+    console.log("AUTH_TRUST_HOST URL:", process.env.AUTH_TRUST_HOST);
+    return config;
+  },
   images: {
     remotePatterns: [
       {
